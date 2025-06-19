269a3fc9ca3970f411f5040cc9be831a45ba5d8c
jgraybrite <jgray@brite.com>
2025-06-19 12:54:46 -0400

    More install functions

---
+++ private/Install-Adobe.ps1
@@ -0,0 +1,5 @@
+Function Install-Adobe{
+
+    Install-StoreApp -PackageID "XPDP273C0XHQH2" -Log "AdobeWingetInstall.log"
+    
+}
---
+++ private/Install-O365.ps1
@@ -0,0 +1,21 @@
+Function Install-O365{
+    
+    $DowloadURL = "https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16731-20398.exe"
+    $DownloadPath = "$Global:BasePath\Apps\O365"
+    $DownloadFile = "$DownloadPath\O365-Installer.exe"
+
+    if (!(Test-Path $DownloadPath)) {
+        New-Item -ItemType "Directory" -Path $DownloadPath
+    }
+    
+    Write-Host "Downloading 365 Apps"
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest $DowloadURL -OutFile $DownloadFile
+    Write-Host "Extracting Setup.exe file"
+    Start-Process $DownloadFile -ArgumentList "/quiet /extract:$DownloadPath" -wait
+
+    $O365ConfigDest = "C:\IT\O365\configuration-Office365-x64.xml"
+    Write-Host "Installing Office"
+    & C:\IT\O365\setup.exe /configure $O365ConfigDest | Wait-Process
+
+}
