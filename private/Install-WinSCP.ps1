22061192063a3a066d078d03aae945a9fadfb060
jgraybrite <jgray@brite.com>
2025-07-17 08:39:34 -0400

    Update Winscp

--- private/Install-WinSCP.ps1
+++ private/Install-WinSCP.ps1
@@ -1,5 +1,7 @@
 Function Install-WinSCP{
 
-    Install-StoreApp -PackageID "WinSCP.WinSCP" -Log "WinSCPWingetInstall.log"
+    #Install-StoreApp -PackageID "WinSCP.WinSCP" -Log "WinSCPWingetInstall.log"
+
+    Winget install --id WinSCP.WinSCP --silent --accept-source-agreements --accept-package-agreements
     
 }
