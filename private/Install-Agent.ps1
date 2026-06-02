c08a5360b5baf859ba17746827f3721eac2a482a
jgraybrite <jgray@brite.com>
2026-06-02 13:36:07 -0400

    update install Agent to not need the global variables

--- private/Install-Agent.ps1
+++ private/Install-Agent.ps1
@@ -1,8 +1,21 @@
 function Install-Agent {
-    Param(
-        $Token,
-        $Domain
-    )
+
+    [CmdletBinding()]
+    param(
+        # Latest stable version of NefCon installer
+        [Parameter(Mandatory=$True)]
+        [string]$Token,
+
+        [Parameter(Mandatory=$True)]
+        [string]$Domain,
+        # Latest stable version of VDD driver only
+        [Parameter(Mandatory=$false)]
+        [string]$BasePath = "C:\ProgramData\Deployment",
+
+        [Parameter(Mandatory=$false)]
+        [string]$SiteCode = 274
+    );
+    
     $TempPath = "$Global:BasePath\Apps\Agent"
     $DownloadPath = "$TempPath\WindowsAgentSetup.exe"
     $AgentDownload = "https://rmm.$Domain/download/2026.2.0.15/winnt/N-central/WindowsAgentSetup.exe"
